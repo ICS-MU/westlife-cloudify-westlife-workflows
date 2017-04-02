76e3e67a142751dd5b0bc9559914fb6c42850226
Vlastimil Holer <holer@ics.muni.cz>
2017-04-02 12:00:54 +0200

    Testing detection of current instances

--- cloudify_westlife_workflows/scale_min_max.py
+++ cloudify_westlife_workflows/scale_min_max.py
@@ -13,6 +13,18 @@ def scale_min_max(ctx,
                  max_instances,
                  ignore_failure=False,
                  **kwargs):
+    graph = ctx.graph_mode()
+    node = ctx.get_node(scalable_entity_name)
+    if not node:
+        raise ValueError("Node {0} doesn't exist".format(scalable_entity_name))
+    if delta == 0:
+        ctx.logger.info('delta parameter is 0, so no scaling will take place.')
+        return
+    host_node = node.host_node
+    scaled_node = host_node if (scale_compute and host_node) else node
+    instances_count = scaled_node.number_of_instances
+    ctx.logger.info("Current number of instances {0}".format(instances_count))
+
     return scale_entity(ctx=ctx,
                         scalable_entity_name=scalable_entity_name,
                         delta=delta,
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     zip_safe=False,
     name='cloudify-westlife-workflows',
-    version='0.0.3',
+    version='0.0.4',
     author='Vlastimil Holer',
     author_email='holer@ics.muni.cz',
     packages=['cloudify_westlife_workflows'],
